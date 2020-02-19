e0a3d8a48e8fed8fab6624711fccc12f0db3803d
Miles Rosen <miles.rosen@gtb.com>
2020-02-19 15:58:01 +0000

    Updates

---
+++ precache-manifest.c538ff5e593589398d6f5f45fb98bb3f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ff9cc59b4cac8366f2c435e8cd90e53",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b724dd2c1c1802c8cb3",
+    "url": "/static/css/main.155a827c.chunk.css"
+  },
+  {
+    "revision": "b6feaeb9dd68843e23ce",
+    "url": "/static/js/2.2417579c.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.2417579c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1b724dd2c1c1802c8cb3",
+    "url": "/static/js/main.5ebcbb29.chunk.js"
+  },
+  {
+    "revision": "91ec5d0c976c915fb5d5",
+    "url": "/static/js/runtime-main.679c150a.js"
+  },
+  {
+    "revision": "dffd2fd517de751e984e6e8a9db3207e",
+    "url": "/static/media/benStokesLogo.dffd2fd5.png"
+  },
+  {
+    "revision": "2d9b09b1746f814fa8c9d2becdf76f3e",
+    "url": "/static/media/indianaHamesLogo.2d9b09b1.png"
+  },
+  {
+    "revision": "d630abcba7ed445d36025738e92d3ff1",
+    "url": "/static/media/playerConnectLogo.d630abcb.png"
+  }
+]);
